7f7e6b64f617cb5b800db3ee5d87beb7f17711f4
Lucas Menéndez <hi@lucasmenendez.me>
2024-02-21 00:10:44 +0100

    new readme, main refactored to handle flags

---
+++ README.md
@@ -0,0 +1,53 @@
+# OnVote Farcaster Bot
+
+Simple [Warpcast](https://warpcast.com/) bot to create polls frames using [farcaster.vote].
+
+## Usage
+
+### Requirements
+
+* Go (>= 1.21.7)
+* Warpcast account for the bot.
+* Farcaster Hub HTTP Enpoint (**optional, a public one is used by default*).
+
+### Basic example 
+
+```sh
+go run cmd/votebot/main.go \
+    -botFid <existing_user_id> \
+    -privateKey <user_signer_private_key>
+```
+
+### Use your own hub
+
+```sh
+go run cmd/votebot/main.go \
+    -botFid <existing_user_id> \
+    -privateKey <user_signer_private_key> \
+    -endpoint <farcaster_hub_http_url>
+```
+
+For example, `debug` over [neynar hub](https://neynar.com/):
+
+```sh
+go run cmd/votebot/main.go \
+    -logLevel debug \
+     -botFid <existing_user_id> \
+    -privateKey <user_signer_private_key> \
+    -endpoint https://hub-api.neynar.com/v1 \
+    -authHeaders api_key \
+    -authKeys <neynar_api_key>
+```
+
+#### Creating a new signer to your FID
+
+The bot will answer to the users with the result of their requests, so it needs the private key of a registered signer for it FID. This signer private key is used to sign bot messages. 
+
+To register a new signer and get its private key, follow these steps:
+
+1. Go to the official [frames debugger](https://debugger.framesjs.org/debug) and complete the login with Warpcast.
+    1. Click on `Sign in with farcaster (costs warps once, works with remote frames and other libs)` option.
+    1. The web app will generate a QR code that you must to scan with a mobile phone with Warpcast installed ([official farcaster client](https://www.farcaster.xyz/)) and logged in the bot account.
+    2. If the QR does not work, copy the the link address of the `open url` option and paste it in your phone browser. Ensure that the address is directly accessed and not entered in any search engine.
+    3. The Warpcast will be openned to confirm the signer creation (it costs a few wraps).
+2. Return to the web app and open the `dev-tools`. You will find all the signer information (including its private key) in the local storage.
--- cmd/votebot/main.go
+++ cmd/votebot/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"flag"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
 
@@ -12,38 +14,66 @@ import (
 )
 
 func main() {
-	logLevel := os.Getenv("LOG_LEVEL")
-	if logLevel == "" {
-		logLevel = "info"
+	botFid := flag.Uint64("botFid", 0, "bot fid")
+	endpoint := flag.String("endpoint", "https://hub.freefarcasterhub.com:3281", "API endpoint")
+	authHeaders := flag.String("authHeaders", "", "auth headers")
+	authKeys := flag.String("authKeys", "", "auth keys")
+	privateKey := flag.String("privateKey", "0x0", "private key")
+	coolDown := flag.Duration("cooldown", time.Second*30, "cooldown between casts")
+	logLevel := flag.String("logLevel", "info", "log level")
+	flag.Parse()
+	// check required flags (bot fid and private key)
+	if *botFid == 0 {
+		log.Fatal("bot fid is required")
 	}
-	// init logger
-	log.Init(logLevel, "stdout", nil)
-
+	if *privateKey == "0x0" {
+		log.Fatal("private key is required")
+	}
+	// check auth headers and keys, they must have the same length even if empty
+	if (*authHeaders != "" && *authKeys == "") || (*authHeaders == "" && *authKeys != "") {
+		log.Fatal("if authHeaders is set, authKeys must be set too and viceversa")
+	}
+	// create a map to store the auth headers and keys, parsing the given
+	// strings separated by commas
+	auth := make(map[string]string)
+	headers := strings.Split(*authHeaders, ",")
+	keys := strings.Split(*authKeys, ",")
+	if len(headers) != len(keys) {
+		log.Fatal("authHeaders and authKeys must have the same length")
+	}
+	for i, header := range headers {
+		auth[header] = keys[i]
+	}
+	// init logger with the given log level
+	log.Init(*logLevel, "stdout", nil)
+	// set up the bot with the given configuration
 	voteBot, err := bot.New(bot.BotConfig{
-		BotFID:     0,
-		Endpoint:   "https://hub.freefarcasterhub.com:3281",
-		CoolDown:   time.Second * 30,
-		PrivateKey: "0x0",
+		BotFID:     *botFid,
+		Endpoint:   *endpoint,
+		Auth:       auth,
+		CoolDown:   *coolDown,
+		PrivateKey: *privateKey,
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	// set demo callback to return the URL of the vote app
 	voteBot.SetCallback(func(poll *bot.Poll) (string, error) {
 		log.Infow("poll received", "poll", poll)
 		return "https://farcaster.vote/app", nil
 	})
-
+	// start the bot with a context and a cancel function
 	ctx, cancel := context.WithCancel(context.Background())
 	voteBot.Start(ctx)
-	// wait for SIGTERM
+	// wait for SIGTERM to cancel the context and stop the bot
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	<-c
 	log.Warnf("received SIGTERM, exiting at %s", time.Now().Format(time.RFC850))
 	cancel()
 	log.Info("waiting for routines to end gracefully...")
-	// closing database
+	// stop the bot and with a timeout of 5 seconds to give time to the
+	// routines to end gracefully
 	go func() {
 		voteBot.Stop()
 		log.Debug("all routines ended")
